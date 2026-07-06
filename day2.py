a75a4ce945b6e05015e2ae5cec710a7d7d467cad
somnathsingh712 <somnathsingh9517@gmail.com>
2026-07-06 11:33:57 +0530

    push and pop operation in stack

--- day2.py
+++ day2.py
@@ -1,32 +1,39 @@
 class Empty(Exception):
     pass
-class arraystack:
+
+
+class ArrayStack:
+
     def __init__(self):
-        self.data=[]
+        self._data = []
 
-        def __len__(self):
-            return len(self._data)
-        
-        def is_empty(self):
-            return len(self._data)==0
-        
-        def push(self,e):
-            self._data.append(e)
-        
-        def top(self):
-            if self.is_empty():
-                raise Empty("Stack is Empty")
-            return self.data[-1]
-        
-        def pop(self):
-            if self.is_empty():
-                raise Empty("Stack is Empty")
-            return self._data.pop()
+    def __len__(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def push(self, e):
+        self._data.append(e)
+
+    def top(self):
+        if self.is_empty():
+            raise Empty("Stack is Empty")
+        return self._data[-1]
+
+    def pop(self):
+        if self.is_empty():
+            raise Empty("Stack is Empty")
+        return self._data.pop()
 
 
-jack=arraystack()
-jack.top()
+jack = ArrayStack()
+
+# jack.top()   # This will raise an exception because the stack is empty.
+
 jack.push(10)
 jack.push(20)
 jack.push(30)
-jack.pop()
+
+print(jack.pop())
+print(jack.top())
