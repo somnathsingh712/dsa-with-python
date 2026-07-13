04ec180fc3a4cd422defcf7414456b0fbfcd0b8a
somnathsingh712 <somnathsingh9517@gmail.com>
2026-07-13 10:53:04 +0530

    dictionary and list

---
+++ intersectionOfDictionary.py
@@ -0,0 +1,13 @@
+#DICTIONARY
+
+
+# dict1={'zope':'zzzz',"python":'program'}
+# dict2={'python':'program','pearl':'ship'}
+
+# print("intersects: ",list(filter (dict2.__contains__,dict1.keys())))
+
+
+#LIST
+the_new_list=[1,2,3,4,5,6,7]
+the_new_list=[x+23 for x in the_new_list if x>5]
+print(the_new_list)
