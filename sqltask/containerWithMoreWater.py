c008958fb1cc691705b5e6a41d781d13e25d5b19
somnathsingh712 <somnathsingh9517@gmail.com>
2026-07-22 12:45:43 +0530

    container with more water

---
+++ sqltask/containerWithMoreWater.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxArea(self, height):
+        left = 0
+        right = len(height) - 1
+        max_water = 0
+
+        while left < right:
+            width = right - left
+            current_height = min(height[left], height[right])
+            area = width * current_height
+
+            max_water = max(max_water, area)
+
+            # Move the smaller height
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_water
